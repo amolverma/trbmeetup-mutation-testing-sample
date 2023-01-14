5fbdc8c92d974dfab129a8288b66ac9fda134ac4
Amol Verma <1268398+amolverma@users.noreply.github.com>
2023-01-14 14:45:37 +0900

    Support multiple countries' drinking ages

--- spec/lib/customer_spec.rb
+++ spec/lib/customer_spec.rb
@@ -1,24 +1,66 @@
 require 'customer'
 
 describe Customer do
-  let(:customer) { Customer.new(age: age) }
+  let(:customer) { Customer.new(age: age, country: country) }
 
   describe '#allowed_to_buy_alcohol?' do
     subject { customer.allowed_to_buy_alcohol? }
 
-    context 'for adults' do
-      let(:age) { 25 }
-      it { is_expected.to be true }
+    context 'in Japan' do
+      let(:country) { :japan }
+
+      context 'for adults' do
+        let(:age) { 25 }
+        it { is_expected.to be true }
+      end
+
+      context 'for teenagers' do
+        let(:age) { 19 }
+        it { is_expected.to be false }
+      end
+
+      context 'for new adults' do
+        let(:age) { 20 }
+        it { is_expected.to be true }
+      end
     end
 
-    context 'for teenagers' do
-      let(:age) { 19 }
-      it { is_expected.to be false }
+    context 'in France' do
+      let(:country) { :france }
+
+      context 'for adults' do
+        let(:age) { 25 }
+        it { is_expected.to be true }
+      end
+
+      context 'for teenagers' do
+        let(:age) { 16 }
+        it { is_expected.to be false }
+      end
+
+      context 'for new adults' do
+        let(:age) { 18 }
+        it { is_expected.to be true }
+      end
     end
 
-    context 'for new adults' do
-      let(:age) { 20 }
-      it { is_expected.to be true }
+    context 'in the US' do
+      let(:country) { :usa }
+
+      context 'for adults' do
+        let(:age) { 25 }
+        it { is_expected.to be true }
+      end
+
+      context 'for teenagers' do
+        let(:age) { 15 }
+        it { is_expected.to be false }
+      end
+
+      context 'for new adults' do
+        let(:age) { 21 }
+        it { is_expected.to be true }
+      end
     end
   end
 end
