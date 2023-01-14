3f49a84bb9916131db1f8e39c34dae417e9392ae
Amol Verma <1268398+amolverma@users.noreply.github.com>
2023-01-14 14:43:46 +0900

    Add Customer class

---
+++ spec/lib/customer_spec.rb
@@ -0,0 +1,19 @@
+require 'customer'
+
+describe Customer do
+  let(:customer) { Customer.new(age: age) }
+
+  describe '#allowed_to_buy_alcohol?' do
+    subject { customer.allowed_to_buy_alcohol? }
+
+    context 'for adults' do
+      let(:age) { 25 }
+      it { is_expected.to be true }
+    end
+
+    context 'for teenagers' do
+      let(:age) { 19 }
+      it { is_expected.to be false }
+    end
+  end
+end
